bd5c6c8ebc78736c24f564560b3c310869129d98
Jung Gyu Yoon <yjg30737@gmail.com>
2022-04-05 08:57:09 +0900

    Showing item tooltip more dynamically (depends on window size)

--- pyqt_show_long_text_as_tooltip_list_widget/showLongTextAsToolTipListWidget.py
+++ pyqt_show_long_text_as_tooltip_list_widget/showLongTextAsToolTipListWidget.py
@@ -9,11 +9,28 @@ class ShowLongTextAsToolTipListWidget(QListWidget):
 
     def __initUi(self):
         self.setMouseTracking(True)
+        self.itemEntered.connect(self.__showToolTip)
 
-    def addItem(self, text: str) -> None:
-        item = QListWidgetItem(self)
-        item.setText(text)
+    def addItem(self, aitem) -> None:
+        item = ''
+        text = ''
+        if isinstance(aitem, str):
+            item = QListWidgetItem()
+            text = aitem
+            item.setText(text)
+        elif isinstance(aitem, QListWidgetItem):
+            item = aitem
+            text = item.text()
         self.setItemWidget(item, QWidget())
+        super().addItem(item)
         item_width = self.fontMetrics().boundingRect(text).width()
         if item_width > self.width():
             item.setToolTip(text)
+
+    def __showToolTip(self, item: QListWidgetItem):
+        item_width = self.visualItemRect(item).width()
+        width = self.width()
+        if item_width > width:
+            item.setToolTip(item.text())
+        else:
+            item.setToolTip('')
